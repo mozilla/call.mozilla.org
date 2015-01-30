1c2ddc6439215cb76f73d66b3cee5a7f6d7ef866
Scott <scott@localhost.localdomain>
2015-01-30 13:49:30 -0500

    navigation and better error detection for invalid numbers

--- call-congress.js
+++ call-congress.js
@@ -1,5 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
   var daysLeftElement = document.querySelector(".days-left");
+  var contentContainer = document.querySelector(".content-container");
   var finalDay = new Date(2015, 1, 26);
   var nowDay = new Date();
   var daysLeft = Math.ceil((finalDay - nowDay)/(1000*60*60*24));
@@ -12,19 +13,35 @@ document.addEventListener("DOMContentLoaded", function() {
   var callNumberError = document.querySelector(".phone-error");
   callNumber.addEventListener("input", function() {
     callNumber.classList.remove("invalid");
-    callNumberError.classList.remove("invalid");
+    callNumberError.classList.remove("show");
   });
   var callButton = document.querySelector(".call-now-button");
   callButton.addEventListener("click", function() {
-    var formValid = false;
-    var callForm = document.querySelector(".call-form");
 
-    if (!callNumber.validity.valid) {
-      callNumber.classList.add("invalid");
-      callNumberError.classList.add("invalid");
-      return;
+    function reqListener (e) {
+      var data = JSON.parse(this.responseText);
+      if (!data.queued) {
+        callNumber.classList.add("invalid");
+        callNumberError.classList.add("show");
+        return;
+      }
+
+      contentContainer.classList.remove("page-1");
+      contentContainer.classList.remove("page-call-script");
+      contentContainer.classList.add("page-calling");
     }
-    callForm.setAttribute("action", callForm.getAttribute("action") + callNumber.value);
-    //callForm.submit();
+
+    var url = "https://callcongress.mofostaging.net/create?campaignId=fcc-blanket&userPhone=";
+    var oReq = new XMLHttpRequest();
+    oReq.onload = reqListener;
+    oReq.open("post", url + callNumber.value, true);
+    oReq.send();
+  });
+
+  var openCallscript = document.querySelector(".call-script-link a");
+  openCallscript.addEventListener("click", function() {
+    contentContainer.classList.remove("page-1");
+    contentContainer.classList.add("page-call-script");
+    contentContainer.classList.remove("page-calling");
   });
 });
