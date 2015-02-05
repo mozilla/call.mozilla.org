4905a4d6cf74c965f1a041dfe0b24ec8bc01c676
Ali Al Dallal <ali@alicoding.com>
2015-02-05 09:54:08 -0500

    Better responsive design

--- call-congress.js
+++ call-congress.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
   var finalDay = new Date(2015, 1, 26);
   var nowDay = new Date();
   var daysLeft = Math.ceil((finalDay - nowDay)/(1000*60*60*24));
+  var readyScript = document.querySelector(".ready-to-call");
+  var boolButton1 = false;
+  var boolButton2 = false;
 
   // screen size:
   var w = window,
@@ -20,46 +23,82 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var callNumber = document.querySelector(".call-number");
   var callNumberError = document.querySelector(".phone-error");
+  var callNumber2 = document.querySelector(".call-number-2");
+  var callNumberError2 = document.querySelector(".phone-error-2");
   var callWrapper = document.querySelector(".page-1-wrapper");
+  var spinner = document.querySelector(".fa-spinner");
 
   callNumber.addEventListener("input", function() {
     callNumber.classList.remove("invalid");
     callNumberError.classList.remove("show");
   });
+
   var callButton = document.querySelector(".call-now-button");
   callButton.addEventListener("click", function() {
+    boolButton1 = true;
+    boolButton2 = false;
     callButton.classList.add("waiting");
-    function reqListener (e) {
-      var data = JSON.parse(this.responseText);
-      callButton.classList.remove("waiting");
-      if (data.message !== "queued") {
-        callNumber.classList.add("invalid");
-        callNumberError.classList.add("show");
-        if(x < 520) {
-          callWrapper.style.height = "450px";
-        } else {
-          callWrapper.style.height = "315px";
-        }
-        return;
-      }
-
-      contentContainer.classList.remove("page-1");
-      contentContainer.classList.remove("page-call-script");
-      contentContainer.classList.add("page-calling");
-    }
-
-    var url = "https://callcongress.mofostaging.net/create?campaignId=fcc-blanket&userPhone=";
-    var oReq = new XMLHttpRequest();
-    oReq.onload = reqListener;
-    oReq.open("post", url + callNumber.value, true);
-    oReq.send();
+    makeCall();
   });
 
-  var openCallscript = document.querySelector(".call-script-link a");
-  var readyScript = document.querySelector(".ready-to-call");
-  var page2Wrapper = document.querySelector(".page-2-wrapper");
+  callNumber2.addEventListener("input", function() {
+    callNumber2.classList.remove("invalid");
+    callNumberError2.classList.remove("show");
+  });
 
+  var callButton2 = document.querySelector(".call-now-button-2");
+  callButton2.addEventListener("click", function() {
+    boolButton1 = false;
+    boolButton2 = true;
+    callButton2.classList.add("waiting");
+    spinner.classList.remove("hidden");
+    alert('click')
+    makeCall();
+  });
+
+
+function makeCall() {
+  var url = "https://callcongress.mofostaging.net/create?campaignId=fcc-blanket&userPhone=";
+  var oReq = new XMLHttpRequest();
+  oReq.onload = reqListener;
+  oReq.open("post", url + callNumber.value, true);
+  oReq.send();
+}
+
+function reqListener (e) {
+  var data = JSON.parse(this.responseText);
+  callButton.classList.remove("waiting");
+  callButton2.classList.remove("waiting");
+  spinner.classList.add("hidden");
+  if (data.message !== "queued") {
+    if(boolButton1) {
+      callNumber.classList.add("invalid");
+      callNumberError.classList.add("show");
+    } else {
+      callNumber2.classList.add("invalid");
+      callNumberError2.classList.add("show");
+      callNumberError2.classList.remove("hidden");
+    }
+
+    if(x < 520) {
+      callWrapper.style.height = "450px";
+      readyScript.style.height = "385px";
+    } else {
+      callWrapper.style.height = "315px";
+      readyScript.style.height = "385px";
+    }
+    return;
+  }
+
+  contentContainer.classList.remove("page-1");
+  contentContainer.classList.remove("page-call-script");
+  contentContainer.classList.add("page-calling");
+}
+
+  var openCallscript = document.querySelector(".call-script-link a");
+  var page2Wrapper = document.querySelector(".page-2-wrapper");
   openCallscript.addEventListener("click", function() {
+
     contentContainer.classList.remove("page-1");
     readyScript.classList.remove("hidden");
     page2Wrapper.classList.remove("hidden");
