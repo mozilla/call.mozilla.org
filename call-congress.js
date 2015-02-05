ca173c61cae0809dd92f68bb380ce5c868d45404
Ali Al Dallal <ali@alicoding.com>
2015-02-05 18:28:22 -0500

    Fix issue 5 - error when press tab on input field

--- call-congress.js
+++ call-congress.js
@@ -28,27 +28,7 @@ document.addEventListener("DOMContentLoaded", function() {
   var spinner = document.querySelector(".fa-spinner");
 
   $("#phone").mask("(999) 999 - 9999");
-  $("#phone").on("blur", function() {
-    processPhone();
-  });
-
-
   $("#phone1").mask("(999) 999 - 9999");
-  $("#phone1").on("blur", function() {
-    processPhone();
-  });
-
-function processPhone() {
-  var last = $(this).val().substr( $(this).val().indexOf("-") + 1 );
-  if( last.length == 3 ) {
-    var move = $(this).val().substr( $(this).val().indexOf("-") - 1, 1 );
-    var lastfour = move + last;
-    var first = $(this).val().substr( 0, 9 );
-
-    $(this).val( first + '-' + lastfour );
-  }
-
-}
 
   callNumber.addEventListener("input", function() {
     callNumber.classList.remove("invalid");
